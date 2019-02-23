639659922e00f314aa928de74e74387ad8bcb046
Pedro Neves <pedro.hfn7@gmail.com>
2019-02-23 10:58:45 -0300

    Adding BookControls component. Finished building App's static version.

--- src/Book.js
+++ src/Book.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
+import BookControls from './BookControls';
 
 class Book extends Component {
 	render () {
-		const { title, authors=[] } = this.props.book;
+		const { book } = this.props
+		const { title, authors=[] } = book;
 
 		return (
 			<div className="book">
 				<div className="book-top">
 					<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
 					<div className="book-shelf-changer">
-						<select>
-							<option value="move" disabled>Move to...</option>
-							<option value="currentlyReading">Currently Reading</option>
-							<option value="wantToRead">Want to Read</option>
-							<option value="read">Read</option>
-							<option value="none">None</option>
-						</select>
+						<BookControls book={book} />
 					</div>
 				</div>
 				<div className="book-title">{title}</div>
